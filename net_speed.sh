995d0808b9d8274a7aa9a3e8217830a27f0dd5d3
Vasiliy <vasiliy.6393@gmail.com>
2021-06-15 02:10:54 +0500

    Add files via upload

---
+++ net_speed.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+GREP="$(which grep)";
+AWK="$(which awk)";
+IFCONFIG="$(which ifconfig)";
+IFACE="ppp0"; n=0; s=0; p=0;
+
+while true; do
+    p="$s";
+    s="$($IFCONFIG $IFACE | $GREP -P 'RX.*bytes' | $AWK '{print $5}')";
+    if [[ "a$p" != "a0" ]]; then
+        sp=$(($s-$p));
+        if [[ "$sp" -lt "1048576" ]]; then
+            speed=$((($sp*8)/1024));
+            fract=$((($sp*8)%1024));
+            prefix="K";
+        else
+            speed=$((($sp*8/1024)/1024));
+            fract=$((($sp*8/1024)%1024));
+            prefix="M";
+        fi
+        echo "$speed.$fract ${prefix}bps"; break;
+    fi
+    sleep 1;
+done
+
