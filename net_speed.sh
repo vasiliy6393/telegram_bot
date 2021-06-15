c58f4a803076cf95bcf379224e3801717b66b1cc
Vasiliy <vasiliy.6393@gmail.com>
2021-06-15 05:01:31 +0500

    Update net_speed.sh

--- net_speed.sh
+++ net_speed.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
-
 GREP="$(which grep)";
 AWK="$(which awk)";
 IFCONFIG="$(which ifconfig)";
-IFACE="ppp0"; n=0; s=0; p=0;
+IFACE="your_interface"; s=0; p=0; 
 
 for i in {1..2}; do
     p="$s";
     s="$($IFCONFIG $IFACE | $GREP -P 'RX.*bytes' | $AWK '{print $5}')";
     if [[ "a$p" != "a0" ]]; then
-        sp=$(($s-$p));
-        if [[ "$sp" -lt "1048576" ]]; then
-            speed=$((($sp*8)/1024));
-            fract=$((($sp*8)%1024));
-            prefix="K";
+        sp="$((($s-$p)*8))";
+        if [[ "$sp" -lt "1024" ]]; then
+            speed="$(($sp))";
+            fract="00";
+            pref="";
+        elif [[ "$sp" -lt "1048576" ]]; then
+            speed="$((($sp)/1024))";
+            fract="$((($sp)%1024))";
+            pref="K";
         else
-            speed=$((($sp*8/1024)/1024));
-            fract=$((($sp*8/1024)%1024));
-            prefix="M";
+            speed="$((($sp/1024)/1024))";
+            fract="$((($sp/1024)%1024))";
+            pref="M";
         fi
-        echo "$speed.$fract ${prefix}bps"; break;
+        echo "$speed.$fract ${pref}bps"; break;
     fi
     sleep 1;
 done
-
