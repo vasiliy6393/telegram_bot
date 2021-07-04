1cf21b3fa3fe697f3390b09097165cca2adbd290
Vasiliy <vasiliy.6393@gmail.com>
2021-07-05 01:45:26 +0500

    Update telegram_send.sh

--- telegram_send.sh
+++ telegram_send.sh
@@ -11,8 +11,7 @@ msg="$(echo "$1" | $SED 's/%0A/\n/g')";
 
 if [[ "a$attach" != "a" ]]; then
     URL="https://api.telegram.org/$TOKEN/sendDocument";
-    msg="$(echo "$msg" | $URLENCODE)";
-    $CURL -A "$UA" -d chat_id=$CID -d document=@"$attach" -d caption="$msg" "$URL"
+    $CURL -A "$UA" -F chat_id=$CID -F document=@"$attach" -F caption="$msg" "$URL"
     if [[ "a$attachs" != "a" ]]; then
         for i in $attachs; do
             $CURL -s -A "$UA" -F chat_id="$CID" -F document=@"$i" "$URL";
